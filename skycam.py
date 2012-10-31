1747c2f822b7ba3c7b5144ee500bfa77d84ff9cb
T. E. Pickering <tim@saao.ac.za>
2012-10-31 04:05:41 +0200

    fix various bugs and make work well enough for now

--- skycam.py
+++ skycam.py
@@ -5,7 +5,16 @@ import os
 import serial
 import struct
 import pyfits
+import time
 import numpy as np
+
+ser = serial.Serial()
+ser.port = "/dev/tty.PL2303-00002006"
+ser.baudrate = 460800
+ser.timeout = 1
+ser.open()
+
+print "reported baudrate is %d" % ser.baudrate
 
 def checksum(c):
     inv = ~ord(c[0]) & 0xFF
@@ -19,97 +28,113 @@ def checksum(c):
 
     return checksum
 
-ser = serial.Serial()
-ser.port = "/dev/tty.PL2303-00002006"
-ser.baudrate = 460800
-ser.timeout = 1
-ser.open()
+def getImage(exptime, light=False):
+    eres = 1.0e-4
+    e = "%06x" % int(exp/eres)
+    if light:
+        e = e + "00" + "01"
+    else:
+        e = e + "00" + "00"
 
-print "reported baudrate is %d" % ser.baudrate
-
-command = struct.pack("BB", ord('Z'), 0xFF)
-to_send = command + struct.pack("B", checksum(command))
-
-eres = 1.0e-4
-exp = int(60.0/eres)
-
-print "exp time 0x%06x" % exp
-
-e = "%06x" % exp
-
-e = e + "00" + "01"
-#e = "00" + "00" + e
-
-etime = e.decode("hex")
-
-command = "T" + etime
-
-print "checksum: %s" % checksum(command)
-
-to_send = command + struct.pack("B", checksum(command))
-
-print "sending:"
-
-for c in to_send:
-    print c, c.encode("hex")
-
-print ""
-
-ser.write(to_send)
-
-out = "E"
-
-while out != "D":
-    out = ser.read(1)
-    print "got %s" % out
-
-
-print "Getting image...."
-to_send = 'X' + struct.pack("B", checksum('X'))
-ser.write(to_send)
-
-imag_str = bytearray()
-
-f = ser.read(1)
-
-print "Get this at the start: %s" % f
-
-for i in range(307200/4096):
-    npix = 4096
-    print "Reading block %d...." % i
-    block_str = ser.read(npix*2)
-    cam = ser.read(1)
-    print "Camera checksum is 0x%02x" % ord(cam)
-    lrc = 0
-    for b in block_str:
-       lrc ^= ord(b)
-    print "Computed checksum is 0x%02x" % lrc
-    imag_str.extend(block_str)
-    to_send = 'K' + struct.pack("B", checksum('K'))
+    etime = e.decode("hex")
+    command = "T" + etime
+    to_send = command + struct.pack("B", checksum(command))
     ser.write(to_send)
 
-print "at end..."
-cam = ser.read(1)
-print "Camera checksum is 0x%02x" % ord(cam)
-cam = ser.read(1)
-print "Camera checksum is 0x%02x" % ord(cam)
-cam = ser.read(1)
-print "Camera checksum is 0x%02x" % ord(cam)
-cam = ser.read(1)
-print "Camera checksum is 0x%02x" % ord(cam)
-cam = ser.read(1)
-print "Camera checksum is 0x%02x" % ord(cam)
+    print "Exposing %f seconds...." % exptime
+    out = "E"
+    while out != "D":
+        out = ser.read(1)
 
-earr = [z for z in imag_str[::2]]
-oarr = [z for z in imag_str[1::2]]
+    print "Getting image...."
+    to_send = 'X' + struct.pack("B", checksum('X'))
+    ser.write(to_send)
+    imag_str = bytearray()
 
-arr = []
+    # not sure why this is needed. not mentioned in document...
+    f = ser.read(1)
 
-for i in range(len(earr)):
-    s = struct.pack("BB", earr[i], oarr[i])
-    n = struct.unpack("H", s)[0]
-    arr.append(n)
+    # we only use unbinned, full frame
+    for i in range(307200/4096):
+        npix = 4096
+        block_str = ser.read(npix*2)
+        cam = ser.read(1)
+        lrc = 0
+        for b in block_str:
+            lrc ^= ord(b)
+        imag_str.extend(block_str)
+        to_send = 'K' + struct.pack("B", checksum('K'))
+        ser.write(to_send)
 
-imag = np.array(arr, dtype=np.uint16).reshape(480,640)
+    # pull these extra bytes out.  also not sure why...
+    cam = ser.read(1)
+    cam = ser.read(1)
+    cam = ser.read(1)
+    cam = ser.read(1)
+    cam = ser.read(1)
 
-pyfits.writeto("test.fits", np.flipud(imag), clobber=True)
+    # this is ugly and there has to be a better way, but it works.
+    earr = [z for z in imag_str[::2]]
+    oarr = [z for z in imag_str[1::2]]
+    arr = []
+
+    for i in range(len(earr)):
+        s = struct.pack("BB", earr[i], oarr[i])
+        n = struct.unpack("H", s)[0]
+        arr.append(n)
+
+    imag = np.array(arr, dtype=np.int32).reshape(480,640)
+    return np.flipud(imag)
+
+
+ndark = 0
+exp = 60.0
+
+while True:
+    if os.path.isfile("STOP"):
+        os.system("rm STOP")
+        break
+
+    if ndark == 0:
+        print "Getting Dark..."
+        dark = getImage(exp, light=False)
+
+    ndark += 1
+    print "Getting Light..."
+    imag = getImage(exp, light=True)
+
+    now = time.localtime()
+    imag -= dark
+    filename = time.strftime("AllSky_%Y%m%d_%H%M%S.fits")
+    jpg = time.strftime("AllSky_%Y%m%d_%H%M%S.jpg")
+    date = time.strftime("%Y/%m/%d")
+    sast = time.strftime("%H:%M:%S")
+    elabel = "Exposure: %f sec" % exp
+
+    cards = []
+    cards.append(pyfits.createCard("DATEOBS", date, "Date of observation"))
+    cards.append(pyfits.createCard("TIMEOBS", 
+                                   sast, 
+                                   "Time of observation (SAST)"))
+    cards.append(pyfits.createCard("EXPTIME", exp, "Exposure time (s)"))
+    header = pyfits.Header(cards=cards)
+
+    pyfits.writeto(filename, imag, header=header, clobber=True)
+    os.system("convert -fill white -normalize \
+    -draw 'text 10,20 \"%s\"' \
+    -draw 'text 10,40 \"%s\"' \
+    -draw 'text 10,60 \"Exptime: %s sec\"' \
+    %s %s" % (date, sast, exp, filename, jpg))
+    os.system("ln -sf %s AllSkyCurrentImage.JPG" % jpg)
+    os.system("ln -sf %s AllSkyCurrentImage.fits" % filename)
+
+    if ndark > 10:
+        ndark = 0
+
+    if np.median(imag) > 15000.0:
+        exp /= 2.0
+        ndark = 0
+
+    if np.median(imag) < 4000.0 and exp < 60.0:
+        exp *= 2.0
+        ndark = 0
