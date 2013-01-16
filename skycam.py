1d650b5deca60cec4bea938ab0a50ad6c8a2d000
T. E. Pickering <tim@saao.ac.za>
2013-01-16 05:10:52 +0200

    update skycam.py to use the new class

--- skycam.py
+++ skycam.py
@@ -1,108 +1,21 @@
 #!/usr/bin/env python
 
-import sys
 import os
-import serial
-import struct
 import pyfits
+import matplotlib
+import pylab as pl
 import time
 import numpy as np
-import fish
-
-ser = serial.Serial()
-ser.port = "/dev/tty.usbserial-A700dzlT"
-ser.baudrate = 460800
-ser.timeout = 1
-ser.open()
-
-print "reported baudrate is %d" % ser.baudrate
+import AllSky340
 
 
-def checksum(c):
-    inv = ~ord(c[0]) & 0xFF
-    mask = ~(1 << 7)
-    checksum = inv & mask
-    if len(c) > 1:
-        for i in range(1, len(c)):
-            inv = ~ord(c[i]) & 0xFF
-            xor = checksum ^ inv
-            checksum = xor & mask
+matplotlib.use('Agg')
 
-    return checksum
-
-
-def getImage(exptime, light=False):
-    eres = 1.0e-4
-    e = "%06x" % int(exp / eres)
-    if light:
-        e = e + "00" + "01"
-    else:
-        e = e + "00" + "00"
-
-    etime = e.decode("hex")
-    command = "T" + etime
-    to_send = command + struct.pack("B", checksum(command))
-    ser.write(to_send)
-
-    print "Exposing %f seconds...." % exptime
-    out = "E"
-    s = ""
-    while out != "D":
-        out = ser.read(1)
-        fish.animate()
-
-    print "Getting image...."
-    to_send = 'X' + struct.pack("B", checksum('X'))
-    ser.write(to_send)
-    imag_str = bytearray()
-
-    # not sure why this is needed. not mentioned in document...
-    f = ser.read(1)
-
-    # we only use unbinned, full frame
-    for i in range(307200 / 4096):
-        npix = 4096
-        block_str = ser.read(npix * 2)
-        cam = ser.read(1)
-        lrc = 0
-        for b in block_str:
-            lrc ^= ord(b)
-        # if lrc != cam:
-        #     while lrc != cam:
-        #         print "Whoops! resending block %d...." % i
-        #         to_send = 'R' + struct.pack("B", checksum('R'))
-        #         block_str = ser.read(npix*2)
-        #         lrc = 0
-        #         for b in block_str:
-        #             lrc ^= ord(b)
-
-        imag_str.extend(block_str)
-        to_send = 'K' + struct.pack("B", checksum('K'))
-        ser.write(to_send)
-
-    # pull these extra bytes out.  also not sure why...
-    cam = ser.read(1)
-    cam = ser.read(1)
-    cam = ser.read(1)
-    cam = ser.read(1)
-    cam = ser.read(1)
-
-    # this is ugly and there has to be a better way, but it works.
-    earr = [z for z in imag_str[::2]]
-    oarr = [z for z in imag_str[1::2]]
-    arr = []
-
-    for i in range(len(earr)):
-        s = struct.pack("BB", earr[i], oarr[i])
-        n = struct.unpack("H", s)[0]
-        arr.append(n)
-
-    imag = np.array(arr, dtype=np.int32).reshape(480, 640)
-    return np.flipud(imag)
-
+cam = AllSky340(port="/dev/tty.usbserial-A700dzlT",
+                baudrate=460800, timeout=1)
 
 ndark = 0
-exp = 60.0
+exp = 15.0
 
 while True:
     if os.path.isfile("STOP"):
@@ -111,13 +24,12 @@ while True:
 
     try:
         if ndark == 0:
-            print "Getting Dark..."
-            dark = getImage(exp, light=False)
+            dark = cam.getImage(exp, light=False)
             ndark += 1
 
-        print "Getting Light..."
-        imag = getImage(exp, light=True)
+        imag = cam.getImage(exp, light=True)
 
+        # get the time and set up labels and filenames
         now = time.localtime()
         imag -= dark
         filename = time.strftime("AllSky_%Y%m%d_%H%M%S.fits")
@@ -126,6 +38,7 @@ while True:
         sast = time.strftime("%H:%M:%S")
         elabel = "Exposure: %f sec" % exp
 
+        # set up and create the FITS file
         cards = []
         cards.append(pyfits.createCard("DATEOBS", date, "Date of observation"))
         cards.append(pyfits.createCard("TIMEOBS",
@@ -133,13 +46,26 @@ while True:
                                        "Time of observation (SAST)"))
         cards.append(pyfits.createCard("EXPTIME", exp, "Exposure time (s)"))
         header = pyfits.Header(cards=cards)
-
         pyfits.writeto(filename, imag, header=header, clobber=True)
-        os.system("convert -fill white -normalize -gamma 2.0 \
-        -draw 'text 10,20 \"%s\"' \
-        -draw 'text 10,40 \"%s\"' \
-        -draw 'text 10,60 \"%s sec\"' \
-        %s %s" % (date, sast, exp, filename, jpg))
+
+        # set up and create the JPG file
+        fig = pl.figure()
+        ax = fig.add_subplot(111)
+        ax.set_xticklabels([])
+        ax.set_yticklabels([])
+        ax.set_xticks([])
+        ax.set_yticks([])
+        pl.imshow(imag, cmap=matplotlib.cm.gray,
+            norm=matplotlib.colors.LogNorm(vmin=1500, vmax=25000))
+        pl.text(10, 5, date, color='w',
+            verticalalignment='top', fontweight='bold')
+        pl.text(630, 5, sast, color='w',
+            verticalalignment='top', horizontalalignment='right',
+            fontweight='bold')
+        pl.text(10, 475, "%.2g sec" % exp, color='w', fontweight='bold')
+        pl.savefig(jpg, bbox_inches="tight", pad_inches=0.0, quality=95)
+        pl.close()
+
         os.system("ln -sf %s AllSkyCurrentImage.JPG" % jpg)
         os.system("ln -sf %s AllSkyCurrentImage.fits" % filename)
 
